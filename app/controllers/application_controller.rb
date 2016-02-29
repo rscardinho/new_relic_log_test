11c5bac098fb0054382ab49a3e9bd93a2b552162
Reynaldo Scardinho <r.scardinho@gmail.com>
2016-02-29 14:07:27 -0300

    route adjusts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def test
+  def log
     NewRelic::Agent.notice_error '-----------'
+    render nothing: true
   end
 end
